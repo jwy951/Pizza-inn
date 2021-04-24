b9df4248feb1f47c985e1282286dd3c4eac10202
Santa claus <santa@nortpole.com>
2021-04-24 15:30:13 +0300

    add prices

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function small() {
-    myPizza.size = "Small";
-    myPizza.sizeCharge = 150.00;
-    myPizza.crustCharge = 15.00;
-    myPizza.toppingCharge = 10.00;
+function small(size,crust, topping) {
+    this.size = "Small";
+    this.sizeCharge = 150.00;
+    this.crustCharge = 15.00;
+    this.toppingCharge = 10.00;
 };
 
 function medium() {
     myPizza.size = "Medium";
-    myPizza.sizeCharge = 250.00;
+    myPizza.sizeCharge = 200.00;
     myPizza.crustCharge = 20.00;
     myPizza.toppingCharge = 20.00;
 
@@ -15,8 +15,14 @@ function medium() {
 
 function large() {
     myPizza.size = "Large";
-    myPizza.sizeCharge = 300.00;
+    myPizza.sizeCharge = 250.00;
     myPizza.crustCharge = 25.00;
     myPizza.toppingCharge = 30.00;
 
 };
+
+// small.prototype.small = function() {
+//   return this.sizeCharge  +"," +this.crustCharge + "," + this.toppingCharge ;
+// }
+
+
