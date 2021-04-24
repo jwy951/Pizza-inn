dd50adc0f6b58988817f48c63a68d325b092dde1
Santa claus <santa@nortpole.com>
2021-04-24 17:25:49 +0300

    toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 function small(size,crust, topping) {
-    this.size = "Small";
+    // this.size = "Small";
     this.sizeCharge = 150.00;
     this.crustCharge = 15.00;
     this.toppingCharge = 10.00;
 };
 
 function medium() {
-    myPizza.size = "Medium";
-    myPizza.sizeCharge = 200.00;
-    myPizza.crustCharge = 20.00;
-    myPizza.toppingCharge = 20.00;
+    // myPizza.size = "Medium";
+    this.sizeCharge = 200.00;
+    this.crustCharge = 20.00;
+    this.toppingCharge = 20.00;
 
 };
 
 function large() {
-    myPizza.size = "Large";
-    myPizza.sizeCharge = 250.00;
-    myPizza.crustCharge = 25.00;
-    myPizza.toppingCharge = 30.00;
+    // myPizza.size = "Large";
+    this.sizeCharge = 250.00;
+    this.crustCharge = 25.00;
+    this.toppingCharge = 30.00;
 
 };
 
@@ -25,4 +25,21 @@ function large() {
 //   return this.sizeCharge  +"," +this.crustCharge + "," + this.toppingCharge ;
 // }
 
+function delivery() {
 
+    myPizza.delivery = 1.5;
+    prompt("Where would you like to have your pizza delivered. Enter Estate Name");
+    alert("Your delivery fees would be $1.5!")
+
+};
+function checkedToppings() {
+    var checked = 0;
+    if (document.getElementById("CheckDefaultA").checked) { checked++; }
+    if (document.getElementById("CheckDefaultB").checked) { checked++; }
+    if (document.getElementById("CheckDefaultC").checked) { checked++; }
+    if (document.getElementById("CheckDefaultD").checked) { checked++; }
+    if (document.getElementById("CheckDefaultE").checked) { checked++; }
+    if (document.getElementById("CheckDefaultF").checked) { checked++; }
+    myPizza.toppingCharge *= checked;
+    console.log(myPizza.toppingCharge);
+}
