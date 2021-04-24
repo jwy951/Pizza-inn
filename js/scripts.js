2c52d91e706f1d5ae84a2cdab5b2a5a1100a9c43
Santa claus <santa@nortpole.com>
2021-04-24 10:47:42 +0300

    add constructors

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function small() {
+    myPizza.size = "Small";
+    myPizza.sizeCharge = 150.00;
+    myPizza.crustCharge = 15.00;
+    myPizza.toppingCharge = 10.00;
+};
+
+function medium() {
+    myPizza.size = "Medium";
+    myPizza.sizeCharge = 250.00;
+    myPizza.crustCharge = 20.00;
+    myPizza.toppingCharge = 20.00;
+
+};
+
+function large() {
+    myPizza.size = "Large";
+    myPizza.sizeCharge = 300.00;
+    myPizza.crustCharge = 25.00;
+    myPizza.toppingCharge = 30.00;
+
+};
