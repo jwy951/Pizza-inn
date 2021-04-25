f1802240f8c0259442fc9506b17479ffc15106a2
Santa claus <santa@nortpole.com>
2021-04-25 09:41:12 +0300

    add form

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function small() {
-    // this.size = "Small";
+    myPizza.size = "Small";
     myPizza.sizeCharge = 150.00;
     myPizza.crustCharge = 15.00;
     myPizza.toppingCharge = 10.00;
 };
 
 function medium() {
-    // myPizza.size = "Medium";
+    myPizza.size = "Medium";
     myPizza.sizeCharge = 200.00;
     myPizza.crustCharge = 20.00;
     myPizza.toppingCharge = 20.00;
@@ -14,7 +14,7 @@ function medium() {
 };
 
 function large() {
-    // myPizza.size = "Large";
+    myPizza.size = "Large";
     myPizza.sizeCharge = 250.00;
     myPizza.crustCharge = 25.00;
     myPizza.toppingCharge = 30.00;
